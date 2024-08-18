f03c0f19f2443edc9637722264e84f48256aee74
hojiakbar2806 <hojiakbarnasriddinov2006@gmail.com>
2024-08-18 18:40:10 +0500

    test ok

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,18 +6,9 @@ module.exports = {
       interpreter: "python3", // Python interpreteri
       instances: "max", // Ilova ko'paytmasi soni (max CPU yadrolari bo'yicha)
       exec_mode: "cluster", // Clustering rejimida ishga tushiring
+      autorestart: true, // Ilova ishga tushirishdan oldin qayta ishga tushiriladi
+      watch: false, // Ilova ishga tushirishdan oldin qayta ishga tushiriladi
+      max_memory_restart: "1G", // Ilova ishga tushirishdan oldin qayta ishga tushiriladi
     },
   ],
-
-  deploy: {
-    production: {
-      user: "ubuntu", // EC2 serveridagi foydalanuvchi
-      host: "13.60.180.58", // EC2 server IP yoki domeni
-      ref: "origin/main", // Git tarmog'i
-      repo: "https://github.com/hojiakbar2806/fastapi.git", // Git repozitoriy URL
-      path: "/home/ubuntu/fastapi", // Serverdagi yo'l
-      "post-deploy":
-        "poetry install --only main && pm2 reload ecosystem.config.js", // Deploydan keyingi buyruqlar
-    },
-  },
 };
