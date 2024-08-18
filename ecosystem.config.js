fdc8a330edce33ed5172dcd6240e73f060d91196
hojiakbar2806 <hojiakbarnasriddinov2006@gmail.com>
2024-08-18 19:24:49 +0500

    test ok

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: "fastapi", // Ilova nomi
       script: "poetry", // `poetry` yordamida ishga tushiradi
       interpreter: "python3", // Python interpreteri
-      args: "run gunicorn main:app --workers 4 --port 8000 --worker-class uvicorn.workers.UvicornWorker", // Gunicorn uchun parametrlar
+      args: "run gunicorn main:app --workers 4 --bind 0.0.0.0:8000 --worker-class uvicorn.workers.UvicornWorker", // Gunicorn uchun parametrlar
       instances: "max", // CPU yadrolari bo'yicha ilova ko'paytmasi
       exec_mode: "cluster", // Clustering rejimida ishga tushirish
     },
